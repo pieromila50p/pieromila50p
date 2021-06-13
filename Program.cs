6a5467c9dde827e4b48f9fbd8f0d91acf33e08fb
pieromila50p <pieromila50p@gmail.com>
2021-06-13 17:23:22 +0200

    master

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text.Json;
+
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var repositories = await ProcessRepositories();
+            foreach (var repo in repositories)
+                 {
+                    Console.WriteLine(repo.Name);
+                    Console.WriteLine(repo.Description);
+                    Console.WriteLine(repo.GitHubHomeUrl);
+                    Console.WriteLine(repo.Homepage);
+                    Console.WriteLine(repo.Watchers);
+
+                    Console.WriteLine(" Ostatnie wypchniecie" + repo.LastPush);
+                    Console.WriteLine();
+                 }
+        }
+
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            //var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            //var msg = await stringTask;
+            //Console.Write(msg);
+            //foreach (var repo in repositories)
+            //Console.WriteLine(repo.Name);
+
+            return repositories;
+        }
+    }
+}
